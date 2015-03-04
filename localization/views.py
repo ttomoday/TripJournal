f945055fe72ae54a5997f4bc23f19522d94d6f67
Admin <linux@ubuntu.ubuntu-domain>
2015-03-04 08:28:03 +0200

    change language without redirect

--- localization/views.py
+++ localization/views.py
@@ -1,13 +1,12 @@
 from django.shortcuts import  redirect, render_to_response
 from django.contrib import auth
-from django.utils.translation import activate
 from django.utils import translation
 
 # Create your views here.
 def change_language(request):
     if request.method=="POST":
         lan=request.POST.get("language",'en')
-        activate(lan)
+        translation.activate(lan)
         if request.user.is_authenticated():
             user=request.user
             profile=user.profile
@@ -16,31 +15,14 @@ def change_language(request):
         request.session[translation.LANGUAGE_SESSION_KEY] = lan
     return redirect('/settings/')
 
-    
-def change_language_social(request, url_redirect):
-    user=request.user
-    profile=user.profile
-    if profile.user_language:
-        lang=profile.user_language            
-    else:
-        lang=request.LANGUAGE_CODE
-        profile.user_language=lang
-        profile.save()
-    activate(lang)
-    request.session[translation.LANGUAGE_SESSION_KEY] = lang
-    return redirect(url_redirect)
-    
 
-def change_language_social_index(request):
-    user=request.user
-    profile=user.profile
-    if profile.user_language:
-        lang=profile.user_language            
-    else:
-        lang=request.LANGUAGE_CODE
-        profile.user_language=lang
-        profile.save()
-    activate(lang)
-    request.session[translation.LANGUAGE_SESSION_KEY] = lang
-    return redirect('/')
-   
+def activate_user_language(request):
+    if request.user.is_authenticated():
+        user=request.user
+        profile=user.profile
+        if profile.user_language:
+            lang=profile.user_language
+            if lang != request.LANGUAGE_CODE:
+                translation.activate(lang)
+                request.session[translation.LANGUAGE_SESSION_KEY] = lang
+            
